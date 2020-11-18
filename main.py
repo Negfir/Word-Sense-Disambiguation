6060181b4e427e93b04b661ff589c013efbd6f56
Negin Firouzian <negfir@gmail.com>
2020-11-19 01:46:23 +0330

    Final

--- main.py
+++ main.py
@@ -4,21 +4,59 @@ import nltk
 from nltk.wsd import lesk
 from nltk.corpus import wordnet as wn
 from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 from nltk.probability import *
 from nltk.corpus.reader.wordnet import WordNetError
-import difflib
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
 from sklearn.linear_model import LogisticRegression
 from nltk.tokenize import word_tokenize
 import itertools
+import string
+
+def preprocess(instances):
+    for key, instance in instances.items():
+        instance.lemma = instance.lemma.decode("utf-8").lower()
+        res = []
+        stops = set(stopwords.words('english'))
+        lemmatizer = WordNetLemmatizer()
+        for token in instance.context:
+            # token = ''.join([c for c in str(token.decode("utf-8")) if c not in string.punctuation])
+            # token=token.decode("utf-8");
+            if (len(token) > 1) and (token not in stops) :
+
+                res.append(token.lower().decode("utf-8").replace('@', ''))
+        resLemma = []
+        for word in res:
+            # tag = nltk.pos_tag(str(word))
+            # print(tag)
+            # tagged = map(lambda x: (x[0], tagConvert(x[1])), word)
+            token = lemmatizer.lemmatize(word)
+            # token = lemma_tokens(tagged, lemmatizer)
+            token = ''.join([c for c in str(token) if c not in string.punctuation])
+            resLemma.append(token)
+        instance.context = resLemma
+
+
+def tagConvert(nltk_tag):
+    if (nltk_tag==None):
+        return None
+    else:
+        if nltk_tag.startswith('J'):
+            return wn.ADJ
+        elif nltk_tag.startswith('V'):
+            return wn.VERB
+        elif nltk_tag.startswith('NN'):
+            return wn.NOUN
+        elif nltk_tag.startswith('R'):
+            return wn.ADV
+        else:
+            return None
 
 def baseline(instances, keys):
     cnt = 0
     for entry in instances:
         word = instances[entry].lemma
-        m = wn.synsets(word)
-        base = wn.synsets(word, instances[entry].pos)[0].lemmas()
+        base = wn.synsets(word, tagConvert(instances[entry].pos))[0].lemmas()
         # print(wn.synsets(word))
 
         list = []
@@ -29,8 +67,7 @@ def baseline(instances, keys):
             # print (keys[entry],"---",key, "---",list)
             if key in list:
                 cnt += 1
-    baseline_accuracy = float(cnt) / len(instances)
-    print(" Baseline: ", baseline_accuracy * 100)
+    print("Baseline accuracy: ", float(cnt) / len(instances) * 100)
 
 
 def NLTKlesk(instances, keys):
@@ -40,37 +77,20 @@ def NLTKlesk(instances, keys):
     for entry in instances:
         list = []
         # print(instances[entry].context, "--->",instances[entry].lemma)
-        result = (lesk(instances[entry].context, instances[entry].lemma, instances[entry].pos)).lemmas()
+        result = (lesk(instances[entry].context, instances[entry].lemma, tagConvert(instances[entry].pos))).lemmas()
 
         for l in result:
             list.append(l.key())
-        # print(result)
-        # print(lesk(instances[entry].context, instances[entry].lemma))
-        # print(list)
-        # print(keys[entry])
+
         for key in keys[entry]:
             if key in list:
                 sum += 1
     lesk_accuracy = float(sum) / len(instances)
-    print("Lesk accuracy: " + str(lesk_accuracy * 100) + "%")
+    print("Lesk accuracy: " + str(float(sum) / len(instances) * 100))
 
 
-def preprocess(instances):
-    for key, instance in instances.items():
-        instance.lemma = instance.lemma.decode("utf-8").lower()
-        post = []
-        # print(instance.lemma)
-        stops = set(stopwords.words('english'))
-        # print(instance.context)
-        for token in instance.context:
 
-            if len(token) > 1 and token not in stops:
-                post.append(token.decode("utf-8").lower().replace('@', ''))
-        instance.context = post
-        # print(instance.context)
-
-
-def bootstrapping():
+def bootstrapping(dev_instances, test_instances, dev_key, test_key):
     dev_context = []
     dev_lemmas = []
     dev_labels = []
@@ -80,7 +100,7 @@ def bootstrapping():
         dev_labels.append(str(dev_key[entry]))
 
 
-
+    #Initialize the seed set
     seed_context = []
     seed_lemmas = []
     seed_labels = []
@@ -97,7 +117,8 @@ def bootstrapping():
     #         seed_labels.append(str(test_instances[entry]))
 
     loop=0
-    while (loop<5):
+    acc = []
+    while (loop<10):
         loop=loop+1
         vect = CountVectorizer()
         X=vect.fit_transform(seed_context).toarray()
@@ -108,16 +129,23 @@ def bootstrapping():
         probs=clf.predict_proba(dev_X)
         preds = clf.predict(dev_X)
         for i, prob in enumerate(probs):
-            if(max(prob)>0.6):
+            if(max(prob)>0.9):
                 seed_context.append(dev_context[i])
                 seed_lemmas.append((dev_lemmas[i]))
                 seed_labels.append((dev_labels[i]))
         cnt=0;
+
         for i, pred in enumerate(preds):
-            # print(dev_labels[i] , pred)
             if(dev_labels[i] == pred):
                 cnt=cnt+1;
-        print(cnt/len(preds))
+        print("Iteration",loop,":", cnt/len(preds) *100)
+        acc.append(cnt/len(preds) * 100)
+
+    import matplotlib.pyplot as plt
+    plt.plot(acc)
+    plt.ylabel('Accuracy on train set')
+    plt.xlabel('Iterations')
+    plt.show()
 
     test_context = []
     test_lemmas = []
@@ -131,38 +159,26 @@ def bootstrapping():
     test_preds = clf.predict(test_X)
 
     cnt=0
+
     for i, pred in enumerate(test_preds):
         # print(dev_labels[i] , pred)
         if (test_labels[i] == pred):
             cnt = cnt + 1;
-    print("Accuracy on Test",cnt / len(test_preds))
+    print("Bootstrapping accuracy on Test",cnt / len(test_preds) *100)
 
 
-def get_glossary(sense):
-
+def get_examples(sense):
     definition = wn.synset(sense).definition()
-
     examples = wn.synset(sense).examples()
-
-    glossary = []
-
-    glossary.append(word_tokenize(definition))
+    comb = []
+    comb.append(word_tokenize(definition))
     for example in examples:
-        glossary.append(word_tokenize(example))
-
-    merged = list(itertools.chain(*glossary))
-    return merged
-
-
-from collections import Counter
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+        comb.append(word_tokenize(example))
+    return list(itertools.chain(*comb))
 
 
 
-
-
-def fourthWSD(instances, keys):
+def MixedLesk(instances, keys):
     cnt = 0
     for entry in instances:
         word = instances[entry].lemma
@@ -172,11 +188,9 @@ def fourthWSD(instances, keys):
         best_score = 0
 
         for i in range(0, len(senses)):
-            signature = get_glossary(senses[i].name())
+            signature = get_examples(senses[i].name())
             # Jaccard Similarity
             overlap_count = len(set(signature).intersection(set(instances[entry].context)))
-  
-
 
             if overlap_count > best_score:
                 best_score = overlap_count
@@ -191,7 +205,7 @@ def fourthWSD(instances, keys):
             if key in list:
                 cnt += 1
     accuracy = float(cnt) / len(instances)
-    print("Mixed Lesk: ", accuracy * 100)
+    print("Mixed Lesk accuracy: ", accuracy * 100)
 
 
 
@@ -206,13 +220,11 @@ if __name__ == '__main__':
     # IMPORTANT: keys contain fewer entries than the instances; need to remove them
     dev_instances = {k: v for (k, v) in dev_instances.items() if k in dev_key}
     test_instances = {k: v for (k, v) in test_instances.items() if k in test_key}
-    # for entry in dev_instances:
-    #     print(dev_instances[entry].context)
-    #     print(dev_instances[entry].lemma)
-    #     print(dev_key[entry])
+
+    #Models:
     baseline(test_instances, test_key)
     NLTKlesk(test_instances, test_key)
-    fourthWSD(test_instances, test_key)
-    # bootstrapping()
+    MixedLesk(test_instances, test_key)
+    bootstrapping(dev_instances, test_instances, dev_key, test_key)
 
 
